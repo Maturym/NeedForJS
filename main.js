d5026ac7ba4719c46fa7819645af0dc8f2424cea
Maturym <63467560+Maturym@users.noreply.github.com>
2020-09-11 13:14:04 +0300

    добавлены столкновения, поле очков и кнопка старта

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function getQuantityElements(heightElement) {
 
 function startGame(){
     start.classList.add('hide');
+    gameArea.innerHTML = '';
 
     for (let i = 0; i < getQuantityElements(100); i++) {
       const line = document.createElement('div');
@@ -44,13 +45,16 @@ function startGame(){
       enemy.y = - 100 * setting.traffic * (i + 1);
       enemy.style.left = Math.floor((Math.random() * 250)) + 'px';
       enemy.style.top = enemy.y + 'px';
-      enemy.style.background = 'transparent url(../image/enemy2.png) center / cover no-repeat'
+      enemy.style.background = 'transparent url(../image/enemy.png) center / cover no-repeat'
       gameArea.appendChild(enemy);
     }
-
+    setting.score = 0;
     gameArea.classList.remove('hide');
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = '125px';
+    car.style.bottom = '10px';
+    car.style.top = 'auto';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -58,10 +62,13 @@ function startGame(){
 
 function playGame(){
   if (setting.start){
+    setting.score += setting.speed;
+    score.textContent = 'SCORE:' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0){
       setting.x -= setting.speed;
+      // car.classList.add('rotate');
     }
     if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)){
       setting.x += setting.speed;
@@ -106,6 +113,18 @@ function moveRoad() {
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
   enemy.forEach(function(item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom && 
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top) {
+      setting.start = false;
+      start.classList.remove('hide');
+      start.style.top = score.offsetHeight + 'px';
+    }
+
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
 
